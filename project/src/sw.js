f9039d1d4fa4d08925d542b95f67a651ec8ec744
Gonçalo Ramalho <goncaloramalho@Goncalos-iMac-New.local>
2018-04-24 15:25:27 +0200

    Prepare workbox tools
    - Import workbox to sw.js
    - Create workbox-config with workbox cli

--- project/src/sw.js
+++ project/src/sw.js
@@ -13,3 +13,13 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
+
+if (workbox) {
+    console.log('Workbox is working');
+
+    workbox.precaching.precacheAndRoute([]);
+} else {
+    console.log('Workbox not working');
+}
